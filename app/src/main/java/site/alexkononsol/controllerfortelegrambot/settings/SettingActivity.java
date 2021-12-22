de92a32f88f573da60cb88d4ec0d7c3dc42929d4
yarrou <konic2006@yandex.ru>
2021-12-22 11:43:05 +0100

    adding the "back" button to the "Settings" activity toolbar

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/settings/SettingActivity.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/settings/SettingActivity.java
@@ -14,6 +14,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -42,7 +43,8 @@ public class SettingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     //Menu of Toolbar
