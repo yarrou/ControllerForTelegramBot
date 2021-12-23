74f9d73e99814f3de93ff44960481f241c4bde66
yarrou <konic2006@yandex.ru>
2021-12-23 18:38:47 +0100

    a more correct construction of the path for saving the backup file

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/utils/BackupHelper.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/utils/BackupHelper.java
@@ -1,6 +1,8 @@
 package site.alexkononsol.controllerfortelegrambot.utils;
 
 
+import android.os.Environment;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,11 +12,11 @@ import java.io.OutputStreamWriter;
 public class BackupHelper {
     public static void createBackup() throws IOException {
 
-            File programDir = new File("/sdcard/ControllerForTelegramBot");
+            File programDir = new File(Environment.getExternalStorageDirectory().getPath()+"/ControllerForTelegramBot");
             if(!programDir.exists()){
                 programDir.mkdir();
             }
-            File myFile = new File("/sdcard/ControllerForTelegramBot/backup.bp");
+            File myFile = new File(Environment.getExternalStorageDirectory().getPath()+"/ControllerForTelegramBot/backup.bp");
             myFile.createNewFile();
             FileOutputStream fOut = new FileOutputStream(myFile);
             OutputStreamWriter myOutWriter =
