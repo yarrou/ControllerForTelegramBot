212203036e7bef088bda7fe93bb3c065dc11f97c
yarrou <konic2006@yandex.ru>
2022-03-09 15:47:56 +0100

    LogHelper : added methods for getting the name of the log file and the directory where the log file is located; now the value in the settings is responsible for writing logs to the file.

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/logHelper/LogHelper.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/logHelper/LogHelper.java
@@ -1,21 +1,41 @@
 package site.alexkononsol.controllerfortelegrambot.logHelper;
 
+import android.content.Context;
 import android.util.Log;
-
+import java.io.IOException;
 import site.alexkononsol.controllerfortelegrambot.utils.FileUtils;
+import site.alexkononsol.controllerfortelegrambot.utils.SettingsManager;
 
 public class LogHelper {
     public static final String TAG = "ControllerForTelegramBot";
-    public static void logError(Object object,String message,Throwable e){
-        Log.e(TAG,message,e);
-        FileUtils.writeLog(object,object.toString() + " : " + message + " - " + e.getCause());
+    private static final String LOG_NAME = "programLog.txt";
+
+    public static String getLogDirPath(Context context) throws IOException {
+        return context.getExternalFilesDir(null).getCanonicalPath();
     }
-    public static void logError(Object object, String message){
-        Log.e(TAG,message);
-        FileUtils.writeLog(object,object.toString() + " : " + message);
+
+    public static String getLogFilePath(Context context) throws IOException {
+        return getLogDirPath(context) + "/" + LOG_NAME;
     }
-    public static void logDebug(Object object, String message){
-        Log.d(TAG,message);
-        FileUtils.writeLog(object,object.toString() + " : " + message );
+
+    public static void logError(Object object, String message, Throwable e) {
+        Log.e(TAG, message, e);
+        if (SettingsManager.getSettings().isLogging()) {
+            FileUtils.writeLog(object, object.toString() + " : " + message + " - " + e.getCause());
+        }
+    }
+
+    public static void logError(Object object, String message) {
+        Log.e(TAG, message);
+        if (SettingsManager.getSettings().isLogging()) {
+            FileUtils.writeLog(object, object.toString() + " : " + message);
+        }
+    }
+
+    public static void logDebug(Object object, String message) {
+        Log.d(TAG, message);
+        if (SettingsManager.getSettings().isLogging()) {
+            FileUtils.writeLog(object, object.toString() + " : " + message);
+        }
     }
 }
