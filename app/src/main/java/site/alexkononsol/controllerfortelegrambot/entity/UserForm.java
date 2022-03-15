103d0d9d36560714c8f6335eca0c0cac9acf10dc
yarrou <konic2006@yandex.ru>
2022-03-15 17:34:12 +0100

    UserForm : Gson is used instead of ObjectMapper

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/UserForm.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/UserForm.java
@@ -1,8 +1,6 @@
 package site.alexkononsol.controllerfortelegrambot.entity;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.google.gson.Gson;
 import java.io.Serializable;
 
 public class UserForm implements Serializable {
@@ -14,24 +12,16 @@ public class UserForm implements Serializable {
         this.password = password;
     }
 
-
     public String getUserName() {
         return userName;
     }
-
-
     public String getPassword() {
         return password;
     }
+
     @Override
     public String toString() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String userFormJson = "{\"userName\":null,\"password\":null}";
-        try {
-            userFormJson = objectMapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return userFormJson;
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 }
