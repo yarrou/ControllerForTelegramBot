1a4570a70d5487a51381b5ab5fef6b1d321563ca
yarrou <konic2006@yandex.ru>
2022-03-17 21:44:45 +0100

    City : the design of the City object is synchronized with the server

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/City.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/City.java
@@ -3,11 +3,14 @@ package site.alexkononsol.controllerfortelegrambot.entity;
 import com.google.gson.Gson;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 public class City implements Serializable {
     private int id;
     private String name;
     private String text;
+    private Timestamp dateCreated;
+    private Timestamp dateLastModification;
 
     public int getId() {
         return id;
@@ -31,6 +34,22 @@ public class City implements Serializable {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Timestamp getDateLastModification() {
+        return dateLastModification;
+    }
+
+    public void setDateLastModification(Timestamp dateLastModification) {
+        this.dateLastModification = dateLastModification;
     }
 
     public City(String name, String text) {
