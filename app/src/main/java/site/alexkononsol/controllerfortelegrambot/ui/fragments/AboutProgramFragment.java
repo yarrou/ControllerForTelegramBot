ffff7bbdf9626180b2d09a77d1e6821729352c52
yarrou <konic2006@yandex.ru>
2022-03-21 06:44:58 +0100

    AboutProgramFragment : Switch element replaced with SwitchCompat

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/AboutProgramFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/AboutProgramFragment.java
@@ -12,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
@@ -19,7 +20,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,7 +38,7 @@ public class AboutProgramFragment extends Fragment {
     private Button updateButton;
     TextView contentView;
     String newVersion;
-    private Switch autoInstall;
+    private SwitchCompat autoInstall;
     private String path;
 
     @Override
