d1dac3a134d8993fc95574664e0093ca9d5271d5
yarrou <konic2006@yandex.ru>
2022-03-26 07:10:24 +0100

    LoggingSettingsFragment : added processing of clicks on the clear logs button

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/LoggingSettingsFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/LoggingSettingsFragment.java
@@ -10,8 +10,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
+import java.io.File;
 
 import site.alexkononsol.controllerfortelegrambot.R;
 import site.alexkononsol.controllerfortelegrambot.logHelper.LogHelper;
@@ -20,6 +23,8 @@ import site.alexkononsol.controllerfortelegrambot.utils.SettingsManager;
 
 public class LoggingSettingsFragment extends Fragment {
     private CheckBox logging;
+    private Button clearLogsButton;
+    private TextView sizeLogView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,33 +37,36 @@ public class LoggingSettingsFragment extends Fragment {
     public void onStart() {
         super.onStart();
         logging = getView().findViewById(R.id.checkBox_logging);
+        sizeLogView = getView().findViewById(R.id.size_logs_view);
         TextView logFilePathView = getView().findViewById(R.id.logging_file_path_title);
         String logFilePath = format(getString(R.string.logging_file_path), LogHelper.getLogFilePath());
         logFilePathView.setText(logFilePath);
         logging.setText(getString(R.string.logging_checkbox_title));
         logging.setChecked(SettingsManager.getSettings().isLogging());
-        logging.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    public void run() {
-                        SettingsManager.getSettings().setLogging(logging.isChecked());
-                    }
-                }).start();
-            }
-        });
+        logging.setOnClickListener(v -> new Thread(() -> SettingsManager.getSettings().setLogging(logging.isChecked())).start());
+        clearLogsButton = getView().findViewById(R.id.logs_clear_button);
+        clearLogsButton.setOnClickListener(view -> clearLogs());
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        TextView sizeLogView = getView().findViewById(R.id.size_logs_view);
-        sizeLogView.post(new Runnable() {
-            @Override
-            public void run() {
-                String kilobytes = getString(R.string.size_logs);
-                sizeLogView.setText(format(kilobytes,LogHelper.getSizeLogFile()));
-            }
+
+        viewSizeLogs();
+        clearLogsButton.post(() -> clearLogsButton.setEnabled((new File(LogHelper.getLogFilePath())).exists()));
+    }
+
+    private void clearLogs() {
+        File logs = new File(LogHelper.getLogFilePath());
+        logs.delete();
+        viewSizeLogs();
+    }
+
+    private void viewSizeLogs() {
+        sizeLogView.post(() -> {
+            String kilobytes = getString(R.string.size_logs);
+            sizeLogView.setText(format(kilobytes, LogHelper.getSizeLogFile()));
         });
     }
+
 }
