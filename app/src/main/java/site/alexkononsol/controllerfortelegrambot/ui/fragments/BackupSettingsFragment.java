d282b057030f8835f118685b9dad314430f8fbca
yarrou <konic2006@yandex.ru>
2022-04-03 06:34:37 +0200

    BackupSettingsFragment : new fragment for backup settings

---
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/BackupSettingsFragment.java
@@ -0,0 +1,21 @@
+package site.alexkononsol.controllerfortelegrambot.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import site.alexkononsol.controllerfortelegrambot.R;
+
+public class BackupSettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_backup_settings, container, false);
+    }
+}
