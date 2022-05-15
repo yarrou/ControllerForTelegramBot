ead84a9760dd90083eb0655dc95f4f2b552f4c25
Yarrou <konic2884@gmail.com>
2022-05-15 23:30:40 +0200

    City : added a field storing the encoded image of the city

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/City.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/entity/City.java
@@ -11,6 +11,15 @@ public class City implements Serializable {
     private String text;
     private Timestamp dateCreated;
     private Timestamp dateLastModification;
+    private String picture;
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
 
     public int getId() {
         return id;
@@ -56,6 +65,7 @@ public class City implements Serializable {
         this.name = name;
         this.text = text;
     }
+
     @Override
     public String toString() {
         Gson gson = new Gson();
