ed7ebcec4a1585891b3c8e8ee19e9aecdbb8369f
Yarrou <konic2884@gmail.com>
2022-05-22 09:25:03 +0200

    PutFragment : added methods responsible for selecting the image of the City, the selected image is encoded into an object of the String type and assigned to the corresponding field of the variable City object

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
@@ -1,5 +1,10 @@
 package site.alexkononsol.controllerfortelegrambot;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestToServer;
 import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestType;
@@ -22,6 +32,8 @@ import site.alexkononsol.controllerfortelegrambot.utils.TextValidator;
 public class PutFragment extends Fragment {
 
     Button putButton;
+    private static final int GALLERY_REQUEST_CODE = 1;
+    private ImageView cityImage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,7 +47,8 @@ public class PutFragment extends Fragment {
         View view = getView();
         TextView contentView = (TextView) view.findViewById(R.id.putResponse);
         putButton = (Button)view.findViewById(R.id.buttonPut);
-        String host = SettingsManager.getSettings().getHostName();
+        cityImage = getView().findViewById(R.id.city_picture);
+        cityImage.setOnClickListener(v -> pickFromGallery());
         putButton.setOnClickListener(v -> {
             TextView cityNameView = (TextView) view.findViewById(R.id.putRequest);
             TextView cityDescriptionView = (TextView) view.findViewById(R.id.putRequestDescription);
@@ -43,6 +56,12 @@ public class PutFragment extends Fragment {
             if(TextValidator.noEmptyValidation(cityNameView)&&(TextValidator.noEmptyValidation(cityDescriptionView))){
                 String cityName = cityNameView.getText().toString();
                 String cityDescription = cityDescriptionView.getText().toString();
+                BitmapDrawable bitmapDrawable = ((BitmapDrawable) cityImage.getDrawable());
+                Bitmap bitmap = bitmapDrawable .getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] imageInByte = stream.toByteArray();
+                String cityPictureString = new String(Base64.encodeBase64(imageInByte));
                 contentView.setText(getString(R.string.toastLoading));
                 new Thread(() -> {
                     try{
@@ -50,7 +69,7 @@ public class PutFragment extends Fragment {
                         put.addAuthHeader();
                         put.addLangParam();
                         put.addJsonHeaders();
-                        put.setBody(new City(cityName,cityDescription));
+                        put.setBody(new City(cityName,cityDescription,cityPictureString));
                         String content = put.send().getData();
                         contentView.post(() -> contentView.setText(content));
                     }
@@ -72,5 +91,30 @@ public class PutFragment extends Fragment {
         super.onResume();
         String userLogin = SettingsManager.getSettings().getUserName();
         putButton.setEnabled(userLogin != null);
+    }
+    private void pickFromGallery(){
+        //Create an Intent with action as ACTION_PICK
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        //Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Sets the type as image/*. This ensures only components of type image are selected
+        intent.setType("image/*");
+        //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
+        // Launching the Intent
+        startActivityForResult(intent,GALLERY_REQUEST_CODE);
+
+
+    }
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        // Result code is RESULT_OK only if the user selects an Image
+        if (resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST_CODE:
+                    //data.getData returns the content URI for the selected Image
+                    Uri selectedImage = data.getData();
+                    cityImage.setImageURI(selectedImage);
+                    break;
+            }
     }
 }
