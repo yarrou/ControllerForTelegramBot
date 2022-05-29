1be2a2a8324cdb0d4b6a31abd13a1837fe5a54c0
Yarrou <konic2884@gmail.com>
2022-05-29 15:51:04 +0200

    PutFragment : a new field of type CityDao , which can be initialized using the newInstance static method, passing json in the parameter

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -17,23 +18,62 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+
 import org.apache.commons.codec.binary.Base64;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestToServer;
 import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestType;
+import site.alexkononsol.controllerfortelegrambot.dao.CityDao;
 import site.alexkononsol.controllerfortelegrambot.entity.City;
 import site.alexkononsol.controllerfortelegrambot.logHelper.LogHelper;
+import site.alexkononsol.controllerfortelegrambot.utils.CityPictureService;
 import site.alexkononsol.controllerfortelegrambot.utils.Constants;
 import site.alexkononsol.controllerfortelegrambot.utils.SettingsManager;
 import site.alexkononsol.controllerfortelegrambot.utils.TextValidator;
 
 public class PutFragment extends Fragment {
 
-    Button putButton;
+    private CityDao cityDao;
+    private TextView cityNameView;
+    private TextView cityDescriptionView;
+    private static final String ARG_PARAM1 = "jsonFromCityDao";
+    private Button putButton;
     private static final int GALLERY_REQUEST_CODE = 1;
     private ImageView cityImage;
+    private CityPictureService service;
+
+    public static PutFragment newInstance(String jsonFromCityDao) {
+        PutFragment fragment = new PutFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, jsonFromCityDao);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public PutFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            Gson gson = new Gson();
+            cityDao = gson.fromJson(getArguments().getString(ARG_PARAM1), CityDao.class);
+        }
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        Gson gson = new Gson();
+        String jsonFromCityDao = gson.toJson(cityDao);
+        savedInstanceState.putString(ARG_PARAM1,jsonFromCityDao);
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,14 +84,37 @@ public class PutFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        View view = getView();
-        TextView contentView = (TextView) view.findViewById(R.id.putResponse);
-        putButton = (Button)view.findViewById(R.id.buttonPut);
+        TextView contentView = (TextView) getView().findViewById(R.id.putResponse);
+        service = new CityPictureService(getContext());
+        putButton = (Button)getView().findViewById(R.id.buttonPut);
         cityImage = getView().findViewById(R.id.city_picture);
+        cityNameView = (TextView) getView().findViewById(R.id.putRequest);
+        cityDescriptionView = (TextView) getView().findViewById(R.id.putRequestDescription);
+        if (cityDao != null){
+            LogHelper.logDebug(this,"image temp file path is : " + cityDao.getPictureFilePath());
+            cityNameView.post(() -> {
+                cityNameView.setText(cityDao.getName());
+            });
+            cityDescriptionView.post(()->{
+                cityDescriptionView.setText(cityDao.getText());
+            });
+            cityImage.post(() -> {
+                try {
+                    Drawable d = service.getDrawableFromCityDao(cityDao);
+                    cityImage.setImageDrawable(d);
+                } catch (Exception e) {
+                    LogHelper.logError(PutFragment.this, e.getMessage(), e);
+                    e.printStackTrace();
+                }
+            });
+        }
+        else {
+            City city = new City("","",service.getPictureAlsStringFromDrawable(cityImage.getDrawable()));
+            cityDao = new CityDao(city,getContext());
+        }
         cityImage.setOnClickListener(v -> pickFromGallery());
         putButton.setOnClickListener(v -> {
-            TextView cityNameView = (TextView) view.findViewById(R.id.putRequest);
-            TextView cityDescriptionView = (TextView) view.findViewById(R.id.putRequestDescription);
+
             //checking for non-emptiness
             if(TextValidator.noEmptyValidation(cityNameView)&&(TextValidator.noEmptyValidation(cityDescriptionView))){
                 String cityName = cityNameView.getText().toString();
@@ -77,7 +140,7 @@ public class PutFragment extends Fragment {
                         LogHelper.logError(PutFragment.this,ex.getMessage(),ex);
                         contentView.post(() -> {
                             contentView.setText(getString(R.string.error) + ": " + ex.getMessage());
-                            Toast.makeText(view.getContext(), getString(R.string.error), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getView().getContext(), getString(R.string.error), Toast.LENGTH_SHORT).show();
                         });
                     }
                 }).start();
@@ -103,8 +166,6 @@ public class PutFragment extends Fragment {
         intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeTypes);
         // Launching the Intent
         startActivityForResult(intent,GALLERY_REQUEST_CODE);
-
-
     }
     public void onActivityResult(int requestCode,int resultCode,Intent data){
         // Result code is RESULT_OK only if the user selects an Image
@@ -113,8 +174,19 @@ public class PutFragment extends Fragment {
                 case GALLERY_REQUEST_CODE:
                     //data.getData returns the content URI for the selected Image
                     Uri selectedImage = data.getData();
-                    cityImage.setImageURI(selectedImage);
+                    LogHelper.logDebug(this,"run onActivityResult");
+                    cityImage.post(() -> {
+                        Drawable tempDrawable = null;
+                        try {
+                            InputStream inputStream = getActivity().getContentResolver().openInputStream(selectedImage);
+                             tempDrawable = Drawable.createFromStream(inputStream, selectedImage.toString() );
+                        } catch (FileNotFoundException e) {
+                            tempDrawable = getResources().getDrawable(R.drawable.city_drawable);
+                        }
+                        cityDao.setPictureFilePath(service.createPictureFile(tempDrawable));
+                    });
                     break;
             }
     }
+
 }
