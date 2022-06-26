20467af62286783f4e378f6330e0797509e19f87
Yarrou <konic2884@gmail.com>
2022-06-26 18:13:13 +0200

    PutFragment : using City class instead of CityDao, using RetrofitRequestToServer instead of RequestToServer

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/PutFragment.java
@@ -3,54 +3,56 @@ package site.alexkononsol.controllerfortelegrambot;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 
-import org.apache.commons.codec.binary.Base64;
-
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestToServer;
-import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RequestType;
-import site.alexkononsol.controllerfortelegrambot.dao.CityDao;
+import site.alexkononsol.controllerfortelegrambot.connectionsUtils.ServerResponse;
+import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RetrofitRequestToServer;
+import site.alexkononsol.controllerfortelegrambot.connectionsUtils.requests.RetrofitRequestType;
 import site.alexkononsol.controllerfortelegrambot.entity.City;
 import site.alexkononsol.controllerfortelegrambot.logHelper.LogHelper;
-import site.alexkononsol.controllerfortelegrambot.utils.CityPictureService;
-import site.alexkononsol.controllerfortelegrambot.utils.Constants;
 import site.alexkononsol.controllerfortelegrambot.utils.SettingsManager;
 import site.alexkononsol.controllerfortelegrambot.utils.TextValidator;
 
 public class PutFragment extends Fragment {
 
-    private static CityDao cityDao;
+    private City city;
     private TextView cityNameView;
     private TextView cityDescriptionView;
-    private static final String ARG_PARAM1 = "jsonFromCityDao";
+    private static final String ARG_PARAM1 = "jsonFromCity";
     private Button putButton;
     private static final int GALLERY_REQUEST_CODE = 1;
     private ImageView cityImage;
-    private CityPictureService service;
+    //private CityPictureService service;
+    private String pathToImage;
 
-    public static PutFragment newInstance(String jsonFromCityDao) {
+    public static PutFragment newInstance(String jsonFromCity) {
         PutFragment fragment = new PutFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, jsonFromCityDao);
+        args.putString(ARG_PARAM1, jsonFromCity);
         fragment.setArguments(args);
         return fragment;
     }
@@ -63,14 +65,15 @@ public class PutFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             Gson gson = new Gson();
-            cityDao = gson.fromJson(getArguments().getString(ARG_PARAM1), CityDao.class);
+            city = gson.fromJson(getArguments().getString(ARG_PARAM1), City.class);
+            pathToImage = city.getPicture();
         }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         Gson gson = new Gson();
-        String jsonFromCityDao = gson.toJson(cityDao);
+        String jsonFromCityDao = gson.toJson(city);
         savedInstanceState.putString(ARG_PARAM1, jsonFromCityDao);
 
 
@@ -87,24 +90,21 @@ public class PutFragment extends Fragment {
     public void onStart() {
         super.onStart();
         TextView contentView = (TextView) getView().findViewById(R.id.putResponse);
-        service = new CityPictureService(getContext());
         putButton = (Button) getView().findViewById(R.id.buttonPut);
         cityImage = getView().findViewById(R.id.city_picture);
         cityNameView = (TextView) getView().findViewById(R.id.putRequest);
         cityDescriptionView = (TextView) getView().findViewById(R.id.putRequestDescription);
-        if (cityDao != null) {
-            LogHelper.logDebug(this, "image temp file path is : " + cityDao.getPictureFilePath());
-            cityNameView.post(() -> {
-                cityNameView.setText(cityDao.getName());
-            });
-            cityDescriptionView.post(() -> {
-                cityDescriptionView.setText(cityDao.getText());
-            });
+        if (city != null) {
+            LogHelper.logDebug(this, "image file path is : " + city.getPicture());
+            cityNameView.post(() -> cityNameView.setText(city.getName()));
+            cityDescriptionView.post(() -> cityDescriptionView.setText(city.getText()));
 
         } else {
-            City city = new City("", "", service.getPictureAlsStringFromDrawable(cityImage.getDrawable()));
-            cityDao = new CityDao(city, getContext());
+            pathToImage = "android.resource://" + getContext().getApplicationContext().getPackageName() + "/drawable/city_drawable";
+            city = new City("", "", "");
+
         }
+
         cityImage.setOnClickListener(v -> pickFromGallery());
         putButton.setOnClickListener(v -> {
 
@@ -117,25 +117,20 @@ public class PutFragment extends Fragment {
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                 byte[] imageInByte = stream.toByteArray();
-                String cityPictureString = new String(Base64.encodeBase64(imageInByte));
                 contentView.setText(getString(R.string.toastLoading));
-                new Thread(() -> {
-                    try {
-                        RequestToServer put = new RequestToServer(Constants.ENDPOINT_POST_CITY, RequestType.PUT);
-                        put.addAuthHeader();
-                        put.addLangParam();
-                        put.addJsonHeaders();
-                        put.setBody(new City(cityName, cityDescription, cityPictureString));
-                        String content = put.send().getData().toString();
-                        contentView.post(() -> contentView.setText(content));
-                    } catch (Exception ex) {
-                        LogHelper.logError(PutFragment.this, ex.getMessage(), ex);
-                        contentView.post(() -> {
-                            contentView.setText(getString(R.string.error) + ": " + ex.getMessage());
-                            Toast.makeText(getView().getContext(), getString(R.string.error), Toast.LENGTH_SHORT).show();
-                        });
-                    }
-                }).start();
+                ExecutorService executor = Executors.newSingleThreadExecutor();
+                Handler handler = new Handler(Looper.getMainLooper());
+                executor.execute(() -> {
+                    //Background work here
+                    RetrofitRequestToServer requestToServer = new RetrofitRequestToServer();
+                    ServerResponse response = requestToServer.addOrChangeCity(imageInByte, new City(cityName, cityDescription, ""), RetrofitRequestType.PUT);
+                    String content = response.getData().toString();
+                    handler.post(() -> {
+                        //UI Thread work here
+                        contentView.setText(content);
+
+                    });
+                });
             }
 
         });
@@ -146,22 +141,37 @@ public class PutFragment extends Fragment {
         super.onResume();
         String userLogin = SettingsManager.getSettings().getUserName();
         putButton.setEnabled(userLogin != null);
-        cityImage.post(() -> {
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(() -> {
+            //Background work here
+            Bitmap bmp = null;
             try {
-                Drawable d = service.getDrawableFromCityDao(cityDao);
-                cityImage.setImageDrawable(d);
+                URL url = new URL(pathToImage);
+                bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (MalformedURLException me) {
+                try {
+                    bmp = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), Uri.parse(pathToImage));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             } catch (Exception e) {
-                LogHelper.logError(PutFragment.this, e.getMessage(), e);
                 e.printStackTrace();
             }
+
+            Bitmap finalBmp = bmp;
+            handler.post(() -> {
+                if (finalBmp != null) cityImage.setImageBitmap(finalBmp);
+            });
         });
+
     }
 
     private void pickFromGallery() {
         //Create an Intent with action as ACTION_PICK
         Intent intent = new Intent(Intent.ACTION_PICK);
-        //Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // Sets the type as image/*. This ensures only components of type image are selected
         intent.setType("image/*");
         //We pass an extra array with the accepted mime types. This will ensure only components with these MIME types as targeted.
         String[] mimeTypes = {"image/jpeg", "image/png"};
@@ -175,20 +185,16 @@ public class PutFragment extends Fragment {
         if (resultCode == Activity.RESULT_OK)
             switch (requestCode) {
                 case GALLERY_REQUEST_CODE:
-                    //data.getData returns the content URI for the selected Image
                     Uri selectedImage = data.getData();
-                    cityImage.post(() -> {
-                        Drawable tempDrawable = null;
-                        try {
-                            InputStream inputStream = getActivity().getContentResolver().openInputStream(selectedImage);
-                            tempDrawable = Drawable.createFromStream(inputStream, selectedImage.toString());
-                        } catch (FileNotFoundException e) {
-                            tempDrawable = getResources().getDrawable(R.drawable.city_drawable);
-                        }
-                        cityDao.setPictureFilePath(service.createPictureFile(tempDrawable));
-                    });
+                    pathToImage = selectedImage.toString();
+                    LogHelper.logDebug(this, pathToImage);
                     break;
             }
+
     }
 
+    public void setCity(City city) {
+        this.city = city;
+    }
 }
+
