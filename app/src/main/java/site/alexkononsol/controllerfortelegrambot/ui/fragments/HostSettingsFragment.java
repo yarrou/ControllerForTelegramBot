c414ca415bbcac800a28130d3316d8a05c352104
Yarrou <konic2884@gmail.com>
2022-07-07 20:08:32 +0200

    HostSettingsFragment : automatic server availability check

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/HostSettingsFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/HostSettingsFragment.java
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -27,6 +31,34 @@ import site.alexkononsol.controllerfortelegrambot.utils.TextValidator;
 public class HostSettingsFragment extends Fragment {
     private EditText hostNameView;
     private TextView nameBotView;
+    private Timer timer;
+    private TextWatcher hostNameTextWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable arg0) {
+            // user typed: start the timer
+            timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    // do your actual work here
+                    checkHost();
+                }
+            }, 2000); // 2000ms delay before the timer executes the „run“ method from TimerTask
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // nothing to do here
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // user is typing: reset already started timer (if existing)
+            if (timer != null) {
+                timer.cancel();
+            }
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,27 +71,15 @@ public class HostSettingsFragment extends Fragment {
     public void onStart() {
         super.onStart();
         hostNameView = getView().findViewById(R.id.hostName);
+
         nameBotView = getView().findViewById(R.id.nameBot);
         String host = SettingsManager.getSettings().getHostName();
         if (host == null || host.equals("")) {
             hostNameView.setHint(Constants.DEFAULT_HOST_URL);
         } else hostNameView.setText(SettingsManager.getSettings().getHostName());
-
+        hostNameView.addTextChangedListener(hostNameTextWatcher);
         Button testButton = (Button) getView().findViewById(R.id.buttonSettingsGetNameBot);
-        testButton.setOnClickListener(v -> {
-            if (TextValidator.noEmptyValidation(hostNameView)) {  //checking for non-emptiness
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                Handler handler = new Handler(Looper.getMainLooper());
-                executor.execute(() -> {
-                    //Background work here
-                    String content = getNameBot();
-                    handler.post(() -> {
-                        //UI Thread work here
-                        nameBotView.setText(content);
-                    });
-                });
-            }
-        });
+        testButton.setOnClickListener(v -> checkHost());
     }
 
     private String getNameBot() {
@@ -69,11 +89,31 @@ public class HostSettingsFragment extends Fragment {
         }
         RetrofitRequestToServer requestToServer = new RetrofitRequestToServer();
         ServerResponse response = requestToServer.stringRequest(serverUrl, RetrofitRequestType.NAME);
-        if (response.getCode()==200) {
+        if (response.getCode() == 200) {
             return response.getData().toString();
-        }
-        else {
+        } else {
             return getString(R.string.nameBotNotAnswer);
         }
+    }
+
+    private void checkHost() {
+        if (TextValidator.noEmptyValidation(hostNameView)) {  //checking for non-emptiness
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Handler handler = new Handler(Looper.getMainLooper());
+            executor.execute(() -> {
+                //Background work here
+                String content = getNameBot();
+                handler.post(() -> {
+                    //UI Thread work here
+                    nameBotView.setText(content);
+                });
+            });
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkHost();
     }
 }
