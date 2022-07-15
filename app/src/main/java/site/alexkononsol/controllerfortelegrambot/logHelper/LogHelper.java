c3d120c21a47defa5699ed6ffb74e65fdbb5fbca
Yarrou <konic2884@gmail.com>
2022-07-15 13:33:05 +0200

    LogHelper : redesign for use by the AppHelperService

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/logHelper/LogHelper.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/logHelper/LogHelper.java
@@ -1,9 +1,11 @@
 package site.alexkononsol.controllerfortelegrambot.logHelper;
 
 
+import android.content.Context;
 import android.util.Log;
 
 import java.io.File;
+import java.io.IOException;
 
 
 import site.alexkononsol.controllerfortelegrambot.utils.FileUtils;
@@ -11,49 +13,55 @@ import site.alexkononsol.controllerfortelegrambot.utils.SettingsManager;
 
 public class LogHelper {
     public static final String TAG = "ControllerForTelegramBot";
-    private static final String LOG_NAME = "programLog.txt";
-    private static String logDirPath ;
+    public static final String LOG_NAME = "programLog.txt";
+    private String logDirPath;
 
-    public static String getLogDirPath() {
-        return logDirPath;
+    public LogHelper(Context context) {
+
+        try {
+            logDirPath = context.getExternalFilesDir(null).getCanonicalPath();
+            if (SettingsManager.getSettings().isLogging()) {
+                File programDir = new File(logDirPath);
+                if (!programDir.exists()) {
+                    Log.d("DEBUG", "created log dir");
+                    programDir.mkdir();
+                }
+                File myFile = new File(getLogFilePath());
+                if (!myFile.exists()) {
+                    myFile.createNewFile();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-    public static void setLogDirPath(String logDirPath) {
-        LogHelper.logDirPath = logDirPath;
+
+    public String getLogFilePath() {
+        return logDirPath + "/" + LOG_NAME;
     }
 
-
-    public static String getLogFilePath(){
-        return getLogDirPath() + "/" + LOG_NAME;
-    }
-
-    public static void logError(Object object, String message, Throwable e) {
-        Log.e(TAG, message, e);
+    public void logError(String className, String message) {
+        Log.e(TAG,className + " : " + message);
         if (SettingsManager.getSettings().isLogging()) {
-            FileUtils.writeLog(object, object.toString() + " : " + message + " - " + e.getCause());
+            FileUtils.writeLog(getLogFilePath(), className + " : " + message);
         }
     }
 
-    public static void logError(Object object, String message) {
-        Log.e(TAG, message);
-        if (SettingsManager.getSettings().isLogging()) {
-            FileUtils.writeLog(object, object.toString() + " : " + message);
-        }
-    }
-
-    public static void logDebug(Object object, String message) {
+    public void logDebug(String className, String message) {
         Log.d(TAG, message);
         if (SettingsManager.getSettings().isLogging()) {
-            FileUtils.writeLog(object, object.toString() + " : " + message);
+            FileUtils.writeLog(getLogFilePath(), className + " : " + message);
         }
     }
 
-    public static String getSizeLogFile(){
+    public String getSizeLogFile() {
         File file = new File(getLogFilePath());
-        long bytes = file.exists()?file.length():0;
+        long bytes = file.exists() ? file.length() : 0;
         long kb = (bytes / 1024);
         long mb = (kb / 1024);
-        if(mb>0) return mb + " mb";
-        else if(kb > 0) return kb + " kb";
+        if (mb > 0) return mb + " mb";
+        else if (kb > 0) return kb + " kb";
         else return bytes + " b";
     }
 }
