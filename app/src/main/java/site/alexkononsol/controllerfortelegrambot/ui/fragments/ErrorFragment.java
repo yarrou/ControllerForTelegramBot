5d0dfa49a9113574a259a5192d1894e40beff950
Yarrou <konic2884@gmail.com>
2022-07-18 20:14:36 +0200

    ErrorFragment : optimization code

--- app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/ErrorFragment.java
+++ app/src/main/java/site/alexkononsol/controllerfortelegrambot/ui/fragments/ErrorFragment.java
@@ -9,30 +9,12 @@ import android.widget.TextView;
 
 import site.alexkononsol.controllerfortelegrambot.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ErrorFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class ErrorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
 
-    // TODO: Rename and change types of parameters
     private String description;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     *
-     * @return A new instance of fragment ErrorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ErrorFragment newInstance(String param1) {
         ErrorFragment fragment = new ErrorFragment();
         Bundle args = new Bundle();
@@ -51,8 +33,6 @@ public class ErrorFragment extends Fragment {
         if (getArguments() != null) {
             description = getArguments().getString(ARG_PARAM1);
         }
-
-
     }
 
     @Override
@@ -66,11 +46,6 @@ public class ErrorFragment extends Fragment {
     public void onStart() {
         super.onStart();
         TextView textView = getView().findViewById(R.id.error_fragment_text);
-        textView.post(new Runnable() {
-            @Override
-            public void run() {
-                textView.setText(description);
-            }
-        });
+        textView.post(() -> textView.setText(description));
     }
 }
